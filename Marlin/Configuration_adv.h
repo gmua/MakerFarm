9f3b21550f5cded56a994a70f83690a41908e6da
Guillermo Muñoz Arzate <gmua@yahoo.com>
2014-10-17 18:49:24 -0500

    Defined Z_HOME_RETRACT_MM to 3

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -215,8 +215,8 @@
 //homing hits the endstop, then retracts by this distance, before it tries to slowly bump again:
 #define X_HOME_RETRACT_MM 5
 #define Y_HOME_RETRACT_MM 5
-#define Z_HOME_RETRACT_MM 2
-//#define QUICK_HOME  //if this is defined, if both x and y are to be homed, a diagonal move will be performed initially.
+#define Z_HOME_RETRACT_MM 3
+#define QUICK_HOME  //if this is defined, if both x and y are to be homed, a diagonal move will be performed initially.
 
 #define AXIS_RELATIVE_MODES {false, false, false, false}
 
